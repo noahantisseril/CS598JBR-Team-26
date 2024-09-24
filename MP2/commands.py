ad54b5699730786c5333ed692278fa4ca96e1f73
LALAYANG <56383562+LALAYANG@users.noreply.github.com>
2024-09-23 22:37:42 -0400

    Update commands.py with vanilla

--- MP2/commands.py
+++ MP2/commands.py
@@ -14,14 +14,16 @@ input_dataset = ""# selected_humaneval_[seed].jsonl
 
 # TODO: add your seed generated in MP1
 seed = "<your_seed>"
+task_1_vanilla_json = "task_1_vanilla" + seed + ".jsonl"
 task_1_json = "task_1_" + seed + ".jsonl"
+task_2_vanilla_json = "task_2_vanilla" + seed + ".jsonl"
 task_2_json = "task_2_" + seed + ".jsonl"
-task_3_json = "task_3_" + seed + ".jsonl"
 
-# Prompt the models, you can create your `MP2/task_1.py, MP2/task_2.py, MP2/task_3.py` by modifying `MP2/task_[ID].py`
-! python3 MP2/task_1.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_json} "True" |& tee task_1_prompt.log
+# Prompt the models, you can create your `MP2/task_1.py, MP2/task_2.py` by modifying `MP2/task_[ID].py`
+! python3 MP2/task_1_vanilla.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_vanilla_json} "True" |& tee Task_1_vanilla.log
+! python3 MP2/task_1.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_json} "True" |& tee Task_1_prompting.log 
+! python3 MP2/task_2_vanilla.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_vanilla} "True" |& tee Task_2_vanilla.log
 ! python3 MP2/task_2.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_json} "True" |& tee task_2_prompt.log
-! python3 MP2/task_3.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_3_json} "True" |& tee task_3_prompt.log
 ! evaluate_functional_correctness {task_3_json} |& tee task_3_evaluate.log
 
 %cd ..
